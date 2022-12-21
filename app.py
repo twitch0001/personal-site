34f639ea3113afc41338771a3a1408b44cfefaea
twitch#7443 <18677171+twitch0001@users.noreply.github.com>
2022-12-21 19:39:58 +0000

    Remove markdown attempts

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+
 import sys
 import json
 import pathlib
 from dataclasses import dataclass
+from datetime import datetime
 
 from flask import Flask, render_template
+# noinspection PyPackageRequirements
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
@@ -22,12 +25,32 @@ class Project:
     preview_image: str
 
 
+@dataclass
+class Blog:
+    page: str  # url of page
+    name: str
+    description: str
+    created_at: int
+
+    @property
+    def pretty_time(self):
+        return datetime.fromtimestamp(self.created_at).strftime('%c')
+
+
 with open("data/projects.json", "rb") as f:
     raw_projects: list[dict] = json.load(f)
     projects: dict = {}
     for p in raw_projects:
         p = Project(**p)
         projects[p.page] = p
+
+
+with open("data/blogs.json", "rb") as f:
+    raw_blogs: list[dict] = json.load(f)
+    blogs: dict = {}
+    for b in raw_blogs:
+        b = Blog(**b)
+        blogs[b.page] = b
 
 
 @app.route("/")
@@ -37,7 +60,7 @@ def home_page():
 
 @app.route("/blog")
 def blog_page():
-    return render_template("page.html")
+    return render_template("blog.html", blogs=list(blogs.values()))
 
 
 @app.route("/projects")
@@ -50,5 +73,12 @@ def project_view(page: str):
     project = projects.get(page)
     if project is None:
         return render_template("404.html")
-    return render_template(f"projects/lobster40.html")
+    return render_template(f"projects/{page}.html", project=project)
 
+
+@app.route("/blog/<page>")
+def blog_view(page: str):
+    blog = blogs.get(page)
+    if blog is None:
+        return render_template("404.html")
+    return render_template(f"blogs/{page}.html", blog=blog)

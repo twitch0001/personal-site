b0744bf7c1116c91064228672cfffa11a7a4f222
twitch#7443 <18677171+twitch0001@users.noreply.github.com>
2022-12-21 04:33:30 +0000

    Get to work on project pages

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import sys
+import json
+import pathlib
+from dataclasses import dataclass
 
 from flask import Flask, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -9,6 +12,23 @@ if sys.platform != "win32":
     app.wsgi_app = ProxyFix(
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     )
+
+
+@dataclass
+class Project:
+    page: str  # url of page
+    name: str
+    description: str
+    preview_image: str
+    markdown: str  # pages/projects/*.md
+
+
+with open("data/projects.json", "rb") as f:
+    raw_projects: list[dict] = json.load(f)
+    projects: dict = {}
+    for p in raw_projects:
+        p = Project(**p)
+        projects[p.page] = p
 
 
 @app.route("/")
@@ -23,4 +43,10 @@ def blog_page():
 
 @app.route("/projects")
 def projects_page():
-    return render_template("projects.html")
+    return render_template("projects.html", projects=list(projects.values()))
+
+
+@app.route("/projects/<page>")
+def project_view(page: str):
+    return render_template("notready.html", page=page)
+

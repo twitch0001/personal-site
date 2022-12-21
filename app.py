4d2ce48ba4e6caa65647b9ddcfb5c5d644fd0a6b
twitch#7443 <18677171+twitch0001@users.noreply.github.com>
2022-12-21 03:58:46 +0000

    Init

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import sys
+
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = Flask(__name__)
+
+if sys.platform != "win32":
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
 
 
 @app.route("/")
